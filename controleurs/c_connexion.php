057adcd04ea69de661075f7aba74bc1c2d2ea185
Warren <bevilacqua.warren@gmail.com>
2021-04-30 01:13:34 +0200

    adding two cases to the controleur in order to hundle the new inscription option

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -5,7 +5,7 @@ $action = $_REQUEST['action'];
 switch ($action) {   
 	
     case 'connexion' :		//fait appel a la vue connexion, soit le formulaire qui permet d’inscrire le login et mot de passe permettant de se connecté
-	    include("vues/v_admin.php");
+	    include 'vues/v_connexion.php';
 	    break;
 
     case 'verifconnexion' :	//verificonnexion fait appel a login et mot de passe ainsi qu’a la fonction situé dans la classe PDO pour vérifier si ces deux champs corresponde avec la base de donnée
@@ -24,11 +24,31 @@ switch ($action) {
             $_SESSION['type'] = 'administrateur';
             header('Location: index.php');
         }else {
-		    echo 'erreur de login ou mot de passe';
+		    $message = 'erreur de login ou mot de passe';
+			include 'vues/v_message.php';
+			include 'vues/v_connexion.php';
 	    }
 	    break;
+		
+	case 'inscription' :
+		include 'vues/v_inscription.php';
+		break;
+	
+	case 'creationCompte' :
+		$raisonSociale = $_REQUEST['raisonSociale'];
+		$login = $_REQUEST['login'];
+		$mdp = $_REQUEST['mdp'];
+		$adresse = $_REQUEST['adresse'];
+		$cp = $_REQUEST['cp'];
+		$ville = $_REQUEST['ville'];
+		$mail = $_REQUEST['mail'];
 
-        
-
+		$pdo->creationCompte($raisonSociale, $login, $mdp, $adresse, $cp, $ville, $mail);
+		
+		$_SESSION['login'] = $login;
+        $_SESSION['type'] = 'client';
+		$_SESSION['inscription'] = 1;
+		header('Location: index.php');
+		break;
 }
 ?>
