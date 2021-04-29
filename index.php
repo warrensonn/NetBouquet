e663370cf421202609ae54cb5d8c0336cfa5bb18
Warren <bevilacqua.warren@gmail.com>
2021-04-30 01:12:17 +0200

    simple syntax modifications

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 session_start();
 require_once("util/fonctions.inc.php");
 require_once("util/class.pdoLafleur.inc.php");
-include("vues/v_entete.php") ;
-include("vues/v_bandeau.php") ;
+include 'vues/v_entete.php';
+include 'vues/v_bandeau.php';
 
 if(!isset($_REQUEST['uc']))
      $uc = 'accueil';
@@ -14,25 +14,24 @@ $pdo = PdoLafleur::getPdoLafleur();
 switch($uc)
 {
 	case 'accueil':
-		include("vues/v_accueil.php");
+		include 'vues/v_accueil.php';
 		break;
 	case 'voirProduits' :
-		include("controleurs/c_voirProduits.php");
+		include 'controleurs/c_voirProduits.php';
 		break;
 	case 'gererPanier' :
-		include("controleurs/c_gestionPanier.php");
+		include 'controleurs/c_gestionPanier.php';
 		break;  
 	case 'seconnecter' :
-		// include("controleurs/c_gestionProduits.php");
-		include("controleurs/c_connexion.php");
+		include 'controleurs/c_connexion.php';
 		break;  
 	case 'gestionProduits':
-		include("controleurs/c_gestionProduits.php");
+		include 'controleurs/c_gestionProduits.php';
 		break;
 	case 'deconnexion' :
-		include("controleurs/c_deconnexion.php");
+		include 'controleurs/c_deconnexion.php';
 		break;	 
 }
-include("vues/v_pied.php") ;
+include 'vues/v_pied.php';
 ?>
 
