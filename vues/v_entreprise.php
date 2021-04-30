d89c758315e1c7c06e4e0cf0be5c245255590131
Warren <bevilacqua.warren@gmail.com>
2021-04-30 22:24:46 +0200

    some filename changes to respect the dev rules

---
+++ vues/v_entreprise.php
@@ -0,0 +1,7 @@
+<div id="accueil" style="margin-left: 40px" style="font-family:Times New Roman, Times, serif;">
+    <h3> Lafleur, le prince des fleurs sur internet </h3>
+    <h4>L’entreprise Lafleur a été fondée en 2002.</h4>
+    Nous vous proposons un large choix de produits pour agrémenter vos bureaux en toutes occasions. <br> <br>
+    
+</div>
+
---
+++ vues/v_listeProduits.php
@@ -0,0 +1,31 @@
+<div id="produits">
+<?php
+	
+foreach( $lesProduits as $unProduit) 
+{
+	$id = $unProduit['id'];
+	$description = $unProduit['description'];
+	$prix=$unProduit['prix'];
+	$image = $unProduit['image'];
+	?>
+	<ul>
+		<li><img src="<?php echo $image ?>" alt=image /></li>
+		<li><?php echo $description ?></li>
+		<li><?php echo " : ".$prix." Euros" ?>
+		<?php
+		if(isset($_SESSION['login']))
+		{ 
+			if ($_SESSION['type']=='administrateur') { ?>	
+				<li><a href=index.php?uc=gestionProduits&categorie=<?php echo $categorie ?>&produit=<?php echo $id ?>&action=Modifier> 
+				<img src="images/modifier.png" TITLE="Modifier"></li></a>
+				<li><a href=index.php?uc=gestionProduits&categorie=<?php echo $categorie ?>&produit=<?php echo $id ?>&action=Supprimer onclick="return confirm('Voulez-vous vraiment retirer cet article ?');"> 
+				<img src="images/supprimer.png" TITLE="Supprimer"></li></a> <?php
+			} else { ?>
+				<li><a href='index.php?uc=voirProduits&categorie=<?php echo $categorie ?>&produit=<?php echo $id ?>&action=ajouterAuPanier'> 
+				<img src="images/mettrepanier.png" TITLE="Ajouter au panier"> </li></a> <?php
+			}		
+		} ?>
+	</ul> <?php		
+}
+?>
+</div>
