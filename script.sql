03621e75d509165d6ef1a3cf92121ade889ef059
Warren <bevilacqua.warren@gmail.com>
2021-05-02 21:48:57 +0200

    Adding hashage of passwordsù

--- script.sql
+++ script.sql
@@ -1,9 +1,9 @@
 
 # -----------------------------------------------------------------------------
-#       TABLE : produit
+#       TABLE : Produit
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS produit
+CREATE TABLE IF NOT EXISTS Produit
  (
   id INT (3) auto_increment NOT NULL,
   description CHAR(50), 
@@ -15,19 +15,19 @@ CREATE TABLE IF NOT EXISTS produit
  ENGINE=InnoDB;
 
 # -----------------------------------------------------------------------------
-#       INDEX DE LA TABLE produit
+#       INDEX DE LA TABLE Produit
 # -----------------------------------------------------------------------------
 
 
 CREATE  INDEX I_FK_Produit_CATEGORIE
-     ON produit (idCategorie ASC);
+     ON Produit (idCategorie ASC);
 
 
 # -----------------------------------------------------------------------------
-#       TABLE : commande
+#       TABLE : Commande
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS commande
+CREATE TABLE IF NOT EXISTS Commande
  (
    id INT AUTO_INCREMENT NOT NULL,
    dateCommande DATE NULL,
@@ -39,10 +39,10 @@ CREATE TABLE IF NOT EXISTS commande
 
 
 # -----------------------------------------------------------------------------
-#       TABLE : categorie
+#       TABLE : Categorie
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS categorie
+CREATE TABLE IF NOT EXISTS Categorie
  (
    id CHAR (3) NOT NULL,
    libelle CHAR (32) NULL, 
@@ -52,10 +52,10 @@ CREATE TABLE IF NOT EXISTS categorie
 
 
 # -----------------------------------------------------------------------------
-#       TABLE : contenir
+#       TABLE : Contenir
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS contenir
+CREATE TABLE IF NOT EXISTS Contenir
  (
 	idCommande INT NOT NULL ,
   idProduit INT (3) NOT NULL, 
@@ -66,24 +66,24 @@ CREATE TABLE IF NOT EXISTS contenir
 
 
 # -----------------------------------------------------------------------------
-#       INDEX DE LA TABLE contenir
+#       INDEX DE LA TABLE Contenir
 # -----------------------------------------------------------------------------
 
 
 CREATE INDEX I_FK_CONTENIR_COMMANDE
-    ON contenir (idCommande ASC);
+    ON Contenir (idCommande ASC);
 
 CREATE INDEX I_FK_CONTENIR_PRODUIT
-    ON contenir (idProduit ASC);
+    ON Contenir (idProduit ASC);
 
 
 # -----------------------------------------------------------------------------
-#       TABLE : typeUtilisateur
+#       TABLE : TypeUtilisateur
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS typeUtilisateur
+CREATE TABLE IF NOT EXISTS TypeUtilisateur
  (
-	 id CHAR (1) NOT NULL,
+	 id INT (1) NOT NULL,
    libelle CHAR (20) NOT NULL, 
    PRIMARY KEY (id) 
  )
@@ -92,15 +92,15 @@ CREATE TABLE IF NOT EXISTS typeUtilisateur
 
 
 # -----------------------------------------------------------------------------
-#       TABLE : compte 
+#       TABLE : Compte 
 # -----------------------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS compte
+CREATE TABLE IF NOT EXISTS Compte
  (
   id INT (10) AUTO_INCREMENT NOT NULL,
   raisonSociale CHAR (50) NOT NULL,
-  login CHAR (20) NOT NULL,
-  mdp CHAR (20) NOT NULL,
+  login CHAR (20) UNIQUE NOT NULL,
+  mdp CHAR (255) NOT NULL,
   adresse CHAR (32) NOT NULL,
   cp CHAR (5) NOT NULL,
   ville CHAR (32) NOT NULL,
@@ -109,31 +109,42 @@ CREATE TABLE IF NOT EXISTS compte
   PRIMARY KEY(id)
  )
  ENGINE=InnoDB;
+
+
+# -----------------------------------------------------------------------------
+#       INDEX DE LA TABLE Contenir
+# -----------------------------------------------------------------------------
+
+
+CREATE INDEX I_FK_COMPTE_TYPEUTILISATEUR
+    ON Compte (idTypeUtilisateur ASC);
 	
+
+
 # -----------------------------------------------------------------------------
 #       CREATION DES REFERENCES DE TABLE
 # -----------------------------------------------------------------------------
 
 
-ALTER TABLE produit 
+ALTER TABLE Produit 
   ADD FOREIGN KEY FK_PRODUIT_CATEGORIE (idCategorie)
-      REFERENCES categorie (id) ;
+      REFERENCES Categorie (id) ;
 
-ALTER TABLE commande 
+ALTER TABLE Commande 
   ADD FOREIGN KEY FK_COMMANDE_COMPTE (idcompte)
-      REFERENCES compte (id) ;
+      REFERENCES Compte (id) ;
 
-ALTER TABLE contenir 
+ALTER TABLE Contenir 
   ADD FOREIGN KEY FK_CONTENIR_COMMANDE (idCommande)
-      REFERENCES commande (id) ;
+      REFERENCES Commande (id) ;
 
-ALTER TABLE contenir 
+ALTER TABLE Contenir 
   ADD FOREIGN KEY FK_CONTENIR_PRODUIT2 (idProduit)
-      REFERENCES produit (id) ;
+      REFERENCES Produit (id) ;
 
-ALTER TABLE compte
+ALTER TABLE Compte
   ADD FOREIGN KEY FK_COMPTE_TYPEUTILISATEUR (idTypeUtilisateur)
-      REFERENCES typeUtilisateur (id) ;
+      REFERENCES TypeUtilisateur (id) ;
 
 
 
@@ -142,53 +153,52 @@ ALTER TABLE compte
 #       CREATION DES LIGNES DES TABLES
 # -----------------------------------------------------------------------------
 
-INSERT INTO categorie VALUES ('fle', 'Fleurs');
-INSERT INTO categorie VALUES ('pla', 'Plantes');
-INSERT INTO categorie VALUES ('com', 'Composition');
+INSERT INTO Categorie VALUES ('fle', 'Fleurs');
+INSERT INTO Categorie VALUES ('pla', 'Plantes');
+INSERT INTO Categorie VALUES ('com', 'Composition');
 
-INSERT INTO typeUtilisateur VALUES (1, 'administrateur');
-INSERT INTO typeUtilisateur VALUES (2, 'client');
+INSERT INTO TypeUtilisateur VALUES (1, 'administrateur');
+INSERT INTO TypeUtilisateur VALUES (2, 'client');
 
-INSERT INTO produit VALUES (1, 'Bouquet de roses multicolores' , 58, 'images/fleurs/comores.gif', 'fle');
-INSERT INTO produit VALUES (2, 'Bouquet de roses rouges', 50, 'images/fleurs/grenadines.gif', 'fle');
-INSERT INTO produit VALUES (3, 'Bouquet de roses jaunes', 78, 'images/fleurs/mariejaune.gif', 'fle');
-INSERT INTO produit VALUES (4, 'Bouquet de petites roses jaunes', 48, 'images/fleurs/mayotte.gif', 'fle');
-INSERT INTO produit VALUES (5, 'Fuseau de roses multicolores', 63, 'images/fleurs/philippines.gif', 'fle');
-INSERT INTO produit VALUES (6, 'Petit bouquet de roses roses', 43, 'images/fleurs/pakopoka.gif', 'fle');
-INSERT INTO produit VALUES (7, 'Panier de roses multicolores', 78, 'images/fleurs/seychelles.gif', 'fle');
+INSERT INTO Produit VALUES (1, 'Bouquet de roses multicolores' , 58, 'images/fleurs/comores.gif', 'fle');
+INSERT INTO Produit VALUES (2, 'Bouquet de roses rouges', 50, 'images/fleurs/grenadines.gif', 'fle');
+INSERT INTO Produit VALUES (3, 'Bouquet de roses jaunes', 78, 'images/fleurs/mariejaune.gif', 'fle');
+INSERT INTO Produit VALUES (4, 'Bouquet de petites roses jaunes', 48, 'images/fleurs/mayotte.gif', 'fle');
+INSERT INTO Produit VALUES (5, 'Fuseau de roses multicolores', 63, 'images/fleurs/philippines.gif', 'fle');
+INSERT INTO Produit VALUES (6, 'Petit bouquet de roses roses', 43, 'images/fleurs/pakopoka.gif', 'fle');
+INSERT INTO Produit VALUES (7, 'Panier de roses multicolores', 78, 'images/fleurs/seychelles.gif', 'fle');
 
-INSERT INTO produit VALUES (8, 'Panier de fleurs variées', 53, 'images/compo/aniwa.gif', 'com');
-INSERT INTO produit VALUES (9, 'Coup de charme jaune', 38, 'images/compo/kos.gif', 'com');
-INSERT INTO produit VALUES (10, 'Bel arrangement de fleurs de saison', 68, 'images/compo/loth.gif', 'com');
-INSERT INTO produit VALUES (11, 'Coup de charme vert', 41, 'images/compo/luzon.gif', 'com');
-INSERT INTO produit VALUES (12, 'Très beau panier de fleurs précieuses', 98, 'images/compo/makin.gif', 'com');
-INSERT INTO produit VALUES (13, 'Bel assemblage de fleurs précieuses', 68, 'images/compo/mosso.gif', 'com');
-INSERT INTO produit VALUES (14, 'Présentation prestigieuse', 128, 'images/compo/rawaki.gif', 'com');
+INSERT INTO Produit VALUES (8, 'Panier de fleurs variées', 53, 'images/compo/aniwa.gif', 'com');
+INSERT INTO Produit VALUES (9, 'Coup de charme jaune', 38, 'images/compo/kos.gif', 'com');
+INSERT INTO Produit VALUES (10, 'Bel arrangement de fleurs de saison', 68, 'images/compo/loth.gif', 'com');
+INSERT INTO Produit VALUES (11, 'Coup de charme vert', 41, 'images/compo/luzon.gif', 'com');
+INSERT INTO Produit VALUES (12, 'Très beau panier de fleurs précieuses', 98, 'images/compo/makin.gif', 'com');
+INSERT INTO Produit VALUES (13, 'Bel assemblage de fleurs précieuses', 68, 'images/compo/mosso.gif', 'com');
+INSERT INTO Produit VALUES (14, 'Présentation prestigieuse', 128, 'images/compo/rawaki.gif', 'com');
 
-INSERT INTO produit VALUES (15, 'Plante fleurie', 43,'images/plantes/antharium.gif', 'pla');
-INSERT INTO produit VALUES (16, 'Pot de phalaonopsis', 58, 'images/plantes/galante.gif', 'pla');
-INSERT INTO produit VALUES (17, 'Assemblage paysagé', 103, 'images/plantes/lifou.gif', 'pla');
-INSERT INTO produit VALUES (18, 'Belle coupe de plantes blanches', 128, 'images/plantes/losloque.gif', 'pla');
-INSERT INTO produit VALUES (19, 'Pot de mitonia mauve', 83, 'images/plantes/papouasi.gif', 'pla');
-INSERT INTO produit VALUES (20, 'Pot de phalaonopsis blanc', 58, 'images/plantes/pionosa.gif', 'pla');
-INSERT INTO produit VALUES (21, 'Pot de phalaonopsis rose mauve', 58, 'images/plantes/sabana.gif', 'pla');
+INSERT INTO Produit VALUES (15, 'Plante fleurie', 43,'images/plantes/antharium.gif', 'pla');
+INSERT INTO Produit VALUES (16, 'Pot de phalaonopsis', 58, 'images/plantes/galante.gif', 'pla');
+INSERT INTO Produit VALUES (17, 'Assemblage paysagé', 103, 'images/plantes/lifou.gif', 'pla');
+INSERT INTO Produit VALUES (18, 'Belle coupe de plantes blanches', 128, 'images/plantes/losloque.gif', 'pla');
+INSERT INTO Produit VALUES (19, 'Pot de mitonia mauve', 83, 'images/plantes/papouasi.gif', 'pla');
+INSERT INTO Produit VALUES (20, 'Pot de phalaonopsis blanc', 58, 'images/plantes/pionosa.gif', 'pla');
+INSERT INTO Produit VALUES (21, 'Pot de phalaonopsis rose mauve', 58, 'images/plantes/sabana.gif', 'pla');
 
-INSERT INTO compte VALUES (1, 'administrateur', 'warrenbev', 'warrenbvad', '28 route des Vallières', '78125', 'Raizeux', 'warren@gmail.com', 1);
-INSERT INTO compte VALUES (2, 'administrateur', 'elsacd', 'elsacdad', '81 route des chataigniers', '78320', 'Levis saint Nom', 'cad.elsa@gmail.com', 1);
-INSERT INTO compte VALUES (3, 'Linag', 'linag75', 'linagcl', '25 route des coins', '75014', 'Paris', 'linag@gmail.com',  2);
-INSERT INTO compte VALUES (4, 'Pasteuri', 'pasteri78', 'pastericl', '4 routes des charmes', '78125', 'Raizeux', 'pasteuri@gmail.com', 2);
+INSERT INTO Compte VALUES (1, 'administrateur', 'warrenbev', SHA2('warrenbvad', 224), '28 route des Vallières', '78125', 'Raizeux', 'warren@gmail.com', 1);
+INSERT INTO Compte VALUES (2, 'administrateur', 'elsacd', SHA2('elsacdad', 224), '81 route des chataigniers', '78320', 'Levis saint Nom', 'cad.elsa@gmail.com', 1);
+INSERT INTO Compte VALUES (3, 'Linag', 'linag75', SHA2('linagcl', 224), '25 route des coins', '75014', 'Paris', 'linag@gmail.com',  2);
+INSERT INTO Compte VALUES (4, 'Pasteuri', 'pasteri', SHA2('pastericl', 224), '4 routes des charmes', '78125', 'Raizeux', 'pasteuri@gmail.com', 2);
 
-INSERT INTO commande VALUES (1, '2021-04-12', 3, 110);
-INSERT INTO commande VALUES (2, '2021-04-20', 4, 150);
-INSERT INTO commande VALUES (3, '2021-04-28', 4, 170);
+INSERT INTO Commande VALUES (1, '2021-04-12', 3, 110);
+INSERT INTO Commande VALUES (2, '2021-04-20', 4, 150);
+INSERT INTO Commande VALUES (3, '2021-04-28', 4, 170);
 
-INSERT INTO contenir VALUES (1, 12, 2);
-INSERT INTO contenir VALUES (1, 3, 1);
-INSERT INTO contenir VALUES (2, 7, 1);
-INSERT INTO contenir VALUES (2, 5, 3);
-INSERT INTO contenir VALUES (3, 8, 2);
-INSERT INTO contenir VALUES (3, 11, 4);
-
+INSERT INTO Contenir VALUES (1, 12, 2);
+INSERT INTO Contenir VALUES (1, 3, 1);
+INSERT INTO Contenir VALUES (2, 7, 1);
+INSERT INTO Contenir VALUES (2, 5, 3);
+INSERT INTO Contenir VALUES (3, 8, 2);
+INSERT INTO Contenir VALUES (3, 11, 4);
 
 
 
