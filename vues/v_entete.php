2db5e21e4a080e565646190de8cfcb8a9a1558c0
Warren <bevilacqua.warren@gmail.com>
2021-05-04 17:59:19 +0200

    Changing page name

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -20,7 +20,7 @@
 <!-- TITRE ET MENUS -->
 <html lang="fr">
 <head>
-<title>Lafleur</title>
+<title>Application NetBouquet</title>
 <meta http-equiv="Content-Language" content="fr">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link href="util/cssGeneral.css" rel="stylesheet" type="text/css">
